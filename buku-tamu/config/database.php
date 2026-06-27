7e86ed27bf9cf1ddbc128bf690f5d592fc6a957f
muyufar <57388811+muyufar@users.noreply.github.com>
2026-06-27 21:34:11 +0700

    Change database credentials in configuration
    Updated database connection settings for the application.

--- buku-tamu/config/database.php
+++ buku-tamu/config/database.php
@@ -3,8 +3,8 @@
 return [
     'host' => '127.0.0.1',
     'port' => '3306',
-    'database' => 'buku_tamu',
-    'username' => 'root',
-    'password' => '',
+    'database' => 'u700125577_santri',
+    'username' => 'u700125577_santri',
+    'password' => 'Landingpage@1990',
     'charset' => 'utf8mb4',
 ];
